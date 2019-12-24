91f3c7852917733025ad2e0fadb21d22c915db82
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-12-24 21:12:02 +0200

    Add new model to generate a mongoose schema

---
+++ generate-mongoose-schema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+/**
+ * take a schema of data then generates and returns mongoose schema
+ *
+ * @param {Object} schema
+ *
+ * @returns {Object}
+ * @throws {TypeError}
+ */
+const generateMongooseSchema = schema => {
+  const cache = {};
+  Object.entries(schema).forEach(([key, type]) => {
+    switch (type) {
+      case 'string':
+        cache[key] = String;
+        break;
+      case 'number':
+        cache[key] = Number;
+        break;
+      case 'boolean':
+        cache[key] = Boolean;
+        break;
+      case 'date':
+        cache[key] = Date;
+        break;
+      default:
+        throw new TypeError(`Invalid datatype ${type} of ${key}`);
+    }
+  }); // end of foreach
+
+  const mongooseSchema = mongoose.Schema(cache);
+
+  const dataModel = mongoose.model('data', mongooseSchema);
+
+  return dataModel;
+};
+
+module.exports = generateMongooseSchema;
